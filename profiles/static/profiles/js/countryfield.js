9595d6f31bbb7c85846fe8ba93a3e160b0d26830
Ruimar Junior <ruimarnjr@gmail.com>
2024-03-02 15:26:05 +0000

    updated profile views, created countryfields.js

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,14 @@
+/* Extracted from the Code Institute's Boutique Ado Walkthrough Project */
+
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#aab7c4');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#aab7c4');
+    } else {
+        $(this).css('color', '#000');
+    }
+});
