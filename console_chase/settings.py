4e66762cb488b4bc01d9c88c5c596e258276c586
Ruimar Junior <ruimarnjr@gmail.com>
2024-03-06 20:35:58 +0000

    add secret key

--- console_chase/settings.py
+++ console_chase/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fgirucmcsd0!#r%9@9#c!vgkb59s@^*_=+4urx_78^#%syr@gp'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-ruimarnjr-consolechase-b6lw2i8441a.ws-eu108.gitpod.io', 'https://consolechase-c0e71dc8b5f6.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['8000-ruimarnjr-consolechase-b6lw2i8441a.ws-eu108.gitpod.io', 'consolechase-c0e71dc8b5f6.herokuapp.com']
 
 
 # Application definition
