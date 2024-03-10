8d98b5e3ae481f7dcaf882b01f2e5616423ea63f
Ruimar Junior <ruimarnjr@gmail.com>
2024-03-10 11:51:55 +0000

    update django version

--- console_chase/settings.py
+++ console_chase/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'my_secret_key'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-ruimarnjr-consolechase-b6lw2i8441a.ws-eu108.gitpod.io', 'consolechase-c0e71dc8b5f6.herokuapp.com', 'localhost']
 
